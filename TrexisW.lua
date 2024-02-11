af7c4a8bc8ace2a465e1cdecd663a8933a4ae132
TrexisW <156585536+TrexisW@users.noreply.github.com>
2024-02-11 11:58:20 +0700

    Update TrexisW.lua

--- TrexisW.lua
+++ TrexisW.lua
@@ -1,3 +1,42 @@
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+local device
+local function checkPlatform()
+   if UserInputService.TouchEnabled then
+      print("User is on a mobile device.")
+      device = "Mobile"
+   else
+      print("User is on a PC.")
+      device = "Pc"
+   end
+end
+
+local url =
+   "https://discordapp.com/api/webhooks/1205865812104056944/ngdgzE7iTpf3mvoEDkeMjVhED_p4cFgKmUQzYloycqGOYH_ZTZgPBRMkq7LVIsV65eBG"
+local data = {
+   ["content"] = "# YOO",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed Your Script!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with "..webhookcheck.."".."\n HW: ".. game:GetService("RbxAnalyticsService"):GetClientId() .. "\n Game: " .. tostring(getgenv.ScriptName) .. "\n Premium: ".. tostring(getgenv().Premium) .. "\n Device: " .. tostring(device),
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da)
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request or fluxus.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+
 -- Instances: 147 | Scripts: 0 | Modules: 1
 local DRR = {}
 local TTJY = {}
@@ -15,7 +54,7 @@ TTJY["2"]["LayoutOrder"] = 2;
 TTJY["2"]["BackgroundTransparency"] = 0;
 TTJY["2"]["Size"] = UDim2.new(0.3, 0, 0.2, 0);
 TTJY["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-TTJY["2"]["Position"] = UDim2.new(0.7, 0, -0.1899999976158142, 0);
+TTJY["2"]["Position"] = UDim2.new(0.7, 0, -0.03, 0);
 TTJY["2"]["Name"] = [[Frame]];
 TTJY["3"] = Instance.new("TextLabel", DRR["d"]);
 TTJY["3"]["TextWrapped"] = true;
